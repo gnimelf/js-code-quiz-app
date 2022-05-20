ebf6ca191e2b647fa3b0fef1a9e252215a01637a
unknown <fezzygo@gmail.com>
2022-05-20 12:25:44 -0400

    added selectors to script.js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,5 @@
-var questions = [""]
+// Setup Selectors
+var questionText = document.querySelector('#question')
+var answerText = document.querySelector('#answers')
+// var button = document.querySelector("answer")
+var timer = document.querySelector("#time");
